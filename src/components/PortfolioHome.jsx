4c8d783ba410973f1e441f676c1188b3424de4df
James <jamesekasiba123@gmail.com>
2026-03-09 17:40:44 +0300

    initial commit

--- src/components/PortfolioHome.jsx
+++ src/components/PortfolioHome.jsx
@@ -279,7 +279,7 @@ const projects = [
                 <div className="profile-ring"></div>
                 <div className="profile-inner">
                   <img
-                    src="/assets/james.jpeg"
+                    src="/assets/jamezz.png"
                     alt="James Ekasiba"
                     className="profile-image"
                   />
